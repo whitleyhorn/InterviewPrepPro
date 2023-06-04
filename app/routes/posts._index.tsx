c55f148434a76f7b9c5957f1b081d83558621677
Whitley Horn <whitleyblaine@protonmail.com>
2023-06-04 14:57:25 -0400

    initialize project and add some blog posts

---
+++ app/routes/posts._index.tsx
@@ -0,0 +1,31 @@
+import { json } from "@remix-run/node";
+import type { LoaderFunction } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { getPostListings } from "~/models/post.server";
+
+type LoaderData = {
+  posts: Awaited<ReturnType<typeof getPostListings>>;
+};
+
+export const loader: LoaderFunction = async () => {
+  const posts = await getPostListings();
+  return json<LoaderData>({ posts });
+};
+
+export default function Posts() {
+  const { posts } = useLoaderData() as LoaderData;
+  return (
+    <main>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
