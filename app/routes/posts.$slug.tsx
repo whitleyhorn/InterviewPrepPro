9ddc3bbdc7f61d40cec1284ce5e4757863b33827
Whitley Horn <whitleyblaine@protonmail.com>
2023-06-07 13:08:57 -0400

    Add markdown to blog post pages

--- app/routes/posts.$slug.tsx
+++ app/routes/posts.$slug.tsx
@@ -1,4 +1,5 @@
 import type { LoaderFunction } from "@remix-run/node";
+import { marked } from "marked";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { getPost } from "~/models/post.server";
@@ -6,14 +7,16 @@ import { getPost } from "~/models/post.server";
 export const loader: LoaderFunction = async ({ params }) => {
   const { slug } = params;
   const post = await getPost(slug);
-  return json({ post });
+  const html = marked(post?.markdown);
+  return json({ post, html });
 };
 
 export default function PostRoute() {
-  const { post } = useLoaderData();
+  const { post, html } = useLoaderData();
   return (
     <main className="mx-auto max-w-4xl">
       <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
 }
